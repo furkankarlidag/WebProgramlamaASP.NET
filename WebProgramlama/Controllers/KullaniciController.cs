198bed92ce777734327634fba5fc53f17013cb3f
Murat Ergin <muratergin415@gmail.com>
2024-05-26 17:51:45 +0300

    Last

--- WebProgramlama/Controllers/KullaniciController.cs
+++ WebProgramlama/Controllers/KullaniciController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using WebProgramlama.Data;
 using WebProgramlama.Models;
 using Zakuska_AI.Data;
@@ -14,52 +18,106 @@ namespace WebProgramlama.Controllers
         private UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private IdentityContext _context;
+        private readonly IConfiguration _configuration;
         readonly static stringSQL strSQL = new stringSQL();
         static int id = 0;
         static string name = "name";
         static string surname = "surname";
         static string mail2 = "email";
 
-        public KullaniciController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IdentityContext context)
+        public KullaniciController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IdentityContext context, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
+            _configuration = configuration;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string token)
         {
-            var mail = TempData["mail"] as string;
-            if(mail == null) {
+            if (string.IsNullOrEmpty(token))
+            {
+                return RedirectToAction("SignIn", "Account");
+            }
+
+            var principal = ValidateToken(token);
+            if (principal == null)
+            {
+                return Unauthorized();
+            }
+
+            var mail = principal.FindFirstValue(ClaimTypes.Email);
+            if (mail == null)
+            {
                 mail = mail2;
             }
+
             var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
             optionsBuilder.UseSqlServer(strSQL.SQLString);
             var context = new IdentityContext(optionsBuilder.Options);
 
-            var kullanici = context.Users
-                      .FirstOrDefault(k => k.Email == mail);
+            var kullanici = context.Users.FirstOrDefault(k => k.Email == mail);
+            if (kullanici == null)
+            {
+                return NotFound();
+            }
+
             id = kullanici.Id;
             name = kullanici.FirstName;
             surname = kullanici.LastName;
             mail2 = kullanici.Email;
+
             User suankiKullanici = new User
             {
                 FirstName = kullanici.FirstName,
                 LastName = kullanici.LastName,
                 Email = kullanici.Email,
             };
+
+            ViewBag.Token = token;
             return View(suankiKullanici);
         }
-        public IActionResult Validate()
+
+        private ClaimsPrincipal ValidateToken(string token)
         {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public IActionResult Validate(string token)
+        {
+            ViewBag.Token = token;
             var mail = TempData["mail"] as string;
             var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
             optionsBuilder.UseSqlServer(strSQL.SQLString);
             var context = new IdentityContext(optionsBuilder.Options);
-            var searchedUser = context.Users.FirstOrDefault(x => x.Email == mail);
 
-            return View(searchedUser);
+            var kullanici = context.Users.FirstOrDefault(x => x.Email == mail);
+            if (kullanici == null)
+            {
+                return NotFound();
+            }
+
+            return View(kullanici);
         }
 
         [HttpPost]
--- WebProgramlama/Program.cs
+++ WebProgramlama/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebProgramlama.Data;
 using Zakuska_AI.Data;
 
@@ -6,17 +9,47 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<IdentityContext>();
-builder.Services.AddIdentity<AppUser,IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
+// JWT Authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+// Session servisini ekleme
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session timeout süresi
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,7 +58,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
