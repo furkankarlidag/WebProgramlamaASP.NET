4ec74a72763fb9e9478417b610bdd3b986e8fe91
Murat Ergin <muratergin415@gmail.com>
2024-05-26 18:31:59 +0300

    Last1

--- WebProgramlama/Controllers/AccountController.cs
+++ WebProgramlama/Controllers/AccountController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebProgramlama.Data;
 using WebProgramlama.Models;
 using Zakuska_AI.Data;
@@ -11,16 +16,18 @@ namespace WebProgramlama.Controllers
     {
         private UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
         private IdentityContext _Context;
         stringSQL strSQL = new stringSQL();
 
 
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IdentityContext context)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IdentityContext context, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _Context = context;
+            _configuration = configuration;
         }
         public IActionResult SignIn()
         {
@@ -70,8 +77,9 @@ namespace WebProgramlama.Controllers
                     {
                         TempData["mail"] = searchedUser.Email;
                         ViewData["SuccessMessage"] = "Giriş başarılı!";
+                        var token = GenerateJwtToken(userMail);
 
-                        return RedirectToAction("Index", "Kullanici");
+                        return RedirectToAction("Index", "Kullanici", new { token });
                     }
                         
                     
@@ -86,6 +94,71 @@ namespace WebProgramlama.Controllers
             return View();
 
         }
+
+        private string GenerateJwtToken(AppUser user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpiryMinutes"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public IActionResult Validate(string token)
+        {
+            ViewBag.Token = token;
+            var mail = TempData["mail"] as string;
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
+            optionsBuilder.UseSqlServer(strSQL.SQLString);
+            var context = new IdentityContext(optionsBuilder.Options);
+
+            var kullanici = context.Users.FirstOrDefault(x => x.Email == mail);
+            if (kullanici == null)
+            {
+                return NotFound();
+            }
+
+            return View(kullanici);
+        }
+
         [HttpPost]
         public async Task<IActionResult> SignUp(SignUpUser user)
         {
