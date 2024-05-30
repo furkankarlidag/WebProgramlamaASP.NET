00dfeaf67d3ccdf905ef073c59bbac9169619b56
Metehan Belli <109311941+Veliashvilii@users.noreply.github.com>
2024-05-30 23:49:00 +0300

    გამარჭობათ ბიჩებო!

--- WebProgramlama/Data/AppUser.cs
+++ WebProgramlama/Data/AppUser.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 
 namespace Zakuska_AI.Data
 {
     public class AppUser : IdentityUser
     {
-      
+      // Burdayım be burdayım!
 
         
 
